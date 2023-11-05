1714d67ab945ac6b05693d6f33290460bcf42c54
Rejens <rejensraya@gmail.com>
2023-11-05 07:26:45 +0545

    errorhandler bug solved

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,6 @@
 export default function errorHandler(err, req, res, next) {
    if (err.name === "JsonWebTokenError") {
-      res.status(401).json({ error: "authentication failed" });
+      return res.status(401).json({ error: "authentication failed" });
    }
    res.status(500).json({ message: err.message });
 }
